45e352bcf8d426e276f96beb3db9660849e7f1f4
IslemFou <fourati.islem@outlook.fr>
2026-04-05 15:17:17 +0200

    installation de React Vite TailwindCSS v4 et postcss

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-slate-900">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        PixelCraftHub : Connecté !
+      </h1>
+    </div>
+  )
+}
+export default App;
